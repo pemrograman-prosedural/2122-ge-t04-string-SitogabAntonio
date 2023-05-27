6f16f49956cd01c0bdfdb90947a16dfb3183cc29
Sitogab Antonio <antonio.girsang@gmail.com>
2023-05-27 22:17:40 +0700

    memperbaiki masa muda

--- t04_01.c
+++ t04_01.c
@@ -1,44 +1,26 @@
-// 12S21015 - Sitogab Antonio Octavianus Girsang
-// 12S21049 - Jesika Audina Purba
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
-int main(int _argv, char **_argc)
-{
-
-    char kata[50];
-    scanf("%s", &kata);
+int main(int argc, char **argv) {
+    char kata[1000];
     int x;
+
+    scanf("%s", kata);
     scanf("%d", &x);
-    int leng = strlen(kata);
-    int number[leng];
-    char num;
 
-    for (int i = 0; i < leng; i++)  {
-         number[i] = (int) kata[i];
-         x = x % 255;
-         number[i] += x;
+    int len = strlen(kata);
+    
+    int number[len];
 
-    }
-    for (int i = 0; i < leng; i++)
-    {
-        if (number[i] < 100){
-            sprintf(num, "%d", number[i]);
-            printf("0%s", num);
-        }   else    {
-            printf("%d", number[i]);
-        }
+    for (int i = 0; i < len; i++) {
+        number[i] = (int)kata[i];
+        number[i] = (number[i] + x) % 256;
     }
 
-    for (int i = 0; i < leng; i++)
-    {
-        kata[i] = (char)number[i];
+    for (int i = 0; i < len; i++) {
+        printf("%03d", number[i]);
     }
-   
- return 0;
 
+    return 0;
 }
-
